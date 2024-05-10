0aaf20f7cf2f6d01adaa8484f812e0c2041ae9fb
Елизавета Владимирова <quokean@bk.ru>
2024-05-10 19:51:41 +0300

    CourseWork

--- Hotelv2/Hotelv2/Models/Role.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Hotelv2.Models
-{
-    public class Role
-    {
-        public int Id { get; set; } // Личный номер
-        public string Post { get; set; } = string.Empty; // Должность
-    }
-}
